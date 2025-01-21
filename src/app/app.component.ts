71406ae13086f351ef8dc6276362eb34b46d8e49
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-21 11:36:55 +0700

    Setup untuk tailwind css dan flowbite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+import { FooterComponent } from "./shared/components/footer/footer.component";
+import { HeaderComponent } from "./shared/components/header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'belajar-angular';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
+
+// NgModule-based Architecture
+// import { Component, OnInit } from '@angular/core';
+// import { initFlowbite } from 'flowbite';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent implements OnInit {
+//   title = 'belajar-angular';
+
+//   ngOnInit(): void {
+//     initFlowbite();
+//   }
+// }
+
